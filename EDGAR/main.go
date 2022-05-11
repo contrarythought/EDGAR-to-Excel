916de0ea0c89fb17a8010f9c30bc02a34b881dc8
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-11 16:24:44 -0400

    Update main.go

--- EDGAR/main.go
+++ EDGAR/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+const CIK_SIZE = 10
+
 type TickerInfo struct {
-	tickerCIK map[string]getInfo.CompanyTicker
+	tickerCIK map[string]string
 }
 
 func main() {
@@ -26,12 +29,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var t TickerInfo = TickerInfo{make(map[string]getInfo.CompanyTicker)}
+	var t TickerInfo = TickerInfo{make(map[string]string)}
 	for _, v := range c.Collection {
-		t.tickerCIK[v.Ticker] = v
+		cikStr := CIKToString(v.CIK)
+		t.tickerCIK[v.Ticker] = cikStr
 	}
 
-	fmt.Printf("%s\n", t.tickerCIK["BNED"].Title)
-	fmt.Printf("%d\n", t.tickerCIK["AAPL"].CIK)
-	fmt.Printf("%s\n", t.tickerCIK["ALOT"].Title)
+	fmt.Printf("%s\n", t.tickerCIK["BNED"])
+	fmt.Printf("%s\n", t.tickerCIK["AAPL"])
+	fmt.Printf("%s\n", t.tickerCIK["ALOT"])
+}
+
+// try with built-in functions?
+func CIKToString(CIK int) string {
+	var ret []byte
+	cikStr := strconv.Itoa(CIK)
+	cikLen := len(cikStr)
+
+	if cikLen < CIK_SIZE {
+		ret = make([]byte, CIK_SIZE)
+		offset := CIK_SIZE - cikLen
+		var i int
+		// prepend return CIK with '0' if cikLen < 10 
+		for i = 0; i < offset; i++ {
+			ret[i] = '0'
+		}
+
+		// copy over the rest of the CIK string
+		for j := 0 ; i < CIK_SIZE; i, j = i + 1, j + 1 {
+			ret[i] = cikStr[j]
+		}
+
+		return string(ret)
+	}	
+
+	return cikStr
 }
