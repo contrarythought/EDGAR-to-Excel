d72703ab1c1a36d0acd5d326c6dd76575c822f9d
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-11 23:31:44 -0400

    Update getInfo.go

--- EDGAR/getInfo/getInfo.go
+++ EDGAR/getInfo/getInfo.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 type CompanyTicker struct {
@@ -17,9 +18,35 @@ type CompanyCollection struct {
 }
 
 const URL = "https://www.sec.gov/files/company_tickers.json"
+const CIK_SIZE = 10
 
 func (c *CompanyCollection) FromJSON(jsonStr []byte) error {
 	return json.Unmarshal(jsonStr, &c.Collection)
+}
+
+func CIKToString(CIK int) string {
+	var ret []byte
+	cikStr := strconv.Itoa(CIK)
+	cikLen := len(cikStr)
+
+	if cikLen < CIK_SIZE {
+		ret = make([]byte, CIK_SIZE)
+		offset := CIK_SIZE - cikLen
+		var i int
+		// prepend return CIK with '0' if cikLen < 10
+		for i = 0; i < offset; i++ {
+			ret[i] = '0'
+		}
+
+		// copy over the rest of the CIK string
+		for j := 0; i < CIK_SIZE; i, j = i+1, j+1 {
+			ret[i] = cikStr[j]
+		}
+
+		return string(ret)
+	}
+
+	return cikStr
 }
 
 func DownloadTickers() (*CompanyCollection, error) {
