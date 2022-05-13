237fbab6140990eb184106f258dbf17e0b839460
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-13 15:10:20 -0400

    Update MU.go

--- EDGAR/MU/MU.go
+++ EDGAR/MU/MU.go
@@ -10,34 +10,30 @@ import (
 const OI_URL = "https://data.sec.gov/api/xbrl/companyconcept/CIK0000723125/us-gaap/OperatingIncomeLoss.json"
 
 type OperatingIncomeLoss struct {
-	CIK         int    `json:"cik"`
-	Taxonomy    string `json:"taxonomy"`
-	Tag         string `json:"tag"`
-	Label       string `json:"label"`
-	Description string `json:"description"`
-	EntityName  string `json:"entityName"`
-	Units       struct {
-		FileData []USD
-	} `json:"units"`
+	CIK         json.Number `json:"cik"`
+	Taxonomy    string      `json:"taxonomy"`
+	Tag         string      `json:"tag"`
+	Label       string      `json:"label"`
+	Description string      `json:"description"`
+	EntityName  string      `json:"entityName"`
+	Units       interface{} `json:"units"`
 }
 
+/*
 type USD struct {
-	Start string `json:"start"`
-	End   string `json:"end"`
-	Val   int    `json:"val"`
-	Accn  string `json:"accn"`
-	FY    int    `json:"fy"`
-	FP    string `json:"fp"`
-	Form  string `json:"form"`
-	Filed string `json:"filed"`
-	Frame string `json:"frame"`
+	Start string      `json:"start"`
+	End   string      `json:"end"`
+	Val   json.Number `json:"val"`
+	Accn  string      `json:"accn"`
+	FY    json.Number `json:"fy"`
+	FP    string      `json:"fp"`
+	Form  string      `json:"form"`
+	Filed string      `json:"filed"`
+	Frame string      `json:"frame"`
 }
+*/
 
-type OICollection struct {
-	Collection map[string]OperatingIncomeLoss
-}
-
-func (oi *OICollection) GetOperatingIncome() error {
+func GetOperatingIncome(o *OperatingIncomeLoss) error {
 	req, err := http.NewRequest("GET", OI_URL, nil)
 	if err != nil {
 		return err
@@ -55,7 +51,7 @@ func (oi *OICollection) GetOperatingIncome() error {
 		return err
 	}
 
-	if err := json.Unmarshal(resp_str, &oi.Collection); err != nil {
+	if err := json.Unmarshal(resp_str, &o); err != nil {
 		return err
 	}
 
