1a066538cd3fe02d42ce3f4d741fc017c9408dda
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-16 12:30:21 -0400

    Update getInfo.go

--- EDGAR/getInfo/getInfo.go
+++ EDGAR/getInfo/getInfo.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type CompanyTicker struct {
@@ -15,6 +16,74 @@ type CompanyTicker struct {
 
 type CompanyCollection struct {
 	Collection map[string]CompanyTicker
+}
+
+type TickerToCIK struct {
+	Collection map[string]string
+}
+
+type Submissions struct {
+	CIK                               string   `json:"cik"`
+	EntityType                        string   `json:"entityType"`
+	SIC                               string   `json:"sic"`
+	SICDescription                    string   `json:"sicDescription"`
+	InsiderTransactionForOwnerExists  int      `json:"insiderTransactionForOwnerExists"`
+	InsiderTransactionForIssuerExists int      `json:"insiderTransactionForIssuerExists"`
+	Name                              string   `json:"name"`
+	Tickers                           []string `json:"tickers"`
+	Exchanges                         []string `json:"exchanges"`
+	EIN                               string   `json:"ein"`
+	Description                       string   `json:"description,omitempty"`
+	Website                           string   `json:"website,omitempty"`
+	InvestorWebsite                   string   `json:"investorWebsite,omitempty"`
+	Category                          string   `json:"category"`
+	FiscalYearEnd                     string   `json:"fiscalYearEnd"`
+	StateOfIncorporation              string   `json:"stateOfIncorporation"`
+	StateOfIncorporationDescription   string   `json:"stateOfIncorporationDescription"`
+	Addresses                         struct {
+		Mailing struct {
+			Street1                   string `json:"street1"`
+			Street2                   string `json:"street2"`
+			City                      string `json:"city"`
+			StateOrCountry            string `json:"stateOrCountry"`
+			ZipCode                   string `json:"zipCode"`
+			StateOrCountryDescription string `json:"stateOrCountryDescription"`
+		} `json:"mailing"`
+		Business struct {
+			Street1                   string `json:"street1"`
+			Street2                   string `json:"street2"`
+			City                      string `json:"city"`
+			StateOrCountry            string `json:"stateOrCountry"`
+			ZipCode                   string `json:"zipCode"`
+			StateOrCountryDescription string `json:"stateOrCountryDescription"`
+		} `json:"business"`
+	} `json:"addresses"`
+	Phone       string   `json:"phone"`
+	Flags       string   `json:"flags,omitempty"`
+	FormerNames []string `json:"formerNames,omitifempty"`
+	Filings     struct {
+		Recent struct {
+			AccessionNumber    []string    `json:"accessionNumber"`
+			FilingDate         []string    `json:"filingDate"`
+			ReportDate         []string    `json:"reportDate"`
+			AcceptanceDateTime []time.Time `json:"acceptanceDateTime"`
+			Act                []string    `json:"act"`
+			Form               []string    `json:"form"`
+			FileNumber         []string    `json:"fileNumber"`
+			FilmNumber         []string    `json:"filmNumber"`
+			Items              []string    `json:"items"`
+			Size               []int       `json:"size"`
+			IsXBRL             []int       `json:"isXBRL"`
+			IsInlineXBRL       []int       `json:"isInlineXBRL"`
+			PrimaryDocument    []string    `json:"primaryDocument"`
+		} `json:"recent"`
+		Files []struct {
+			Name        string `json:"name"`
+			FilingCount int    `json:"filingCount"`
+			FilingFrom  string `json:"filingFrom"`
+			FilingTo    string `json:"filingTo"`
+		} `json:"files"`
+	} `json:"filings"`
 }
 
 const URL = "https://www.sec.gov/files/company_tickers.json"
@@ -49,20 +118,29 @@ func CIKToString(CIK int) string {
 	return cikStr
 }
 
-func DownloadTickers() (*CompanyCollection, error) {
+func initTickerToCIK() *TickerToCIK {
+	return &TickerToCIK{make(map[string]string)}
+}
+
+func DownloadTickers() (*CompanyCollection, *TickerToCIK, error) {
 	coll := &CompanyCollection{}
 
 	resp, err := http.Get(URL)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err := coll.FromJSON(body); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return coll, nil
+	ticker_to_CIK := initTickerToCIK()
+	for k, v := range coll.Collection {
+		ticker_to_CIK.Collection[k] = CIKToString(v.CIK)
+	}
+
+	return coll, ticker_to_CIK, nil
 }
