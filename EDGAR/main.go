828f20e025ce539c3f3e47489595f79f0143c5cc
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-17 14:45:10 -0400

    Update main.go

--- EDGAR/main.go
+++ EDGAR/main.go
@@ -1,115 +1,50 @@
 package main
 
 import (
-	"EDGAR/MU"
+	"EDGAR/getInfo"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 )
-
-const CIK_SIZE = 10
-
-type TickerInfo struct {
-	tickerCIK map[string]string
-}
 
 // example: https://data.sec.gov/api/xbrl/companyconcept/CIK##########/us-gaap/AccountsPayableCurrent.json
 
 func main() {
-	/*
-		res, err := http.Get(getInfo.URL)
-		if err != nil {
-			log.Fatal(err)
-		}
+	if len(os.Args) != 2 {
+		log.Fatalf("<Ticker>")
+	}
 
-		defer res.Body.Close()
+	ticker := strings.ToUpper(os.Args[1])
 
-		var c *getInfo.CompanyCollection
+	res, err := http.Get(getInfo.COMPANY_TICKERS_URL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		c, err = getInfo.DownloadTickers()
-		if err != nil {
-			log.Fatal(err)
-		}
+	defer res.Body.Close()
 
-		var t TickerInfo = TickerInfo{make(map[string]string)}
-		for _, v := range c.Collection {
-			cikStr := getInfo.CIKToString(v.CIK)
-			t.tickerCIK[v.Ticker] = cikStr
-		}
+	var tCIK *getInfo.TickerToCIK
 
-		fmt.Printf("%s\n", t.tickerCIK["MU"])
-	*/
+	tCIK, err = getInfo.DownloadTickers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tickSubmission, err := getInfo.GetSubmissions(tCIK.Collection[ticker])
+
+	jsonStr, err := json.MarshalIndent(tickSubmission, "", "	")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	f, err := os.Create("res.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var oi MU.OperatingIncomeLoss
-	if err := MU.GetOperatingIncome(&oi); err != nil {
-		log.Fatal(err)
-	}
-
-	s, err := json.MarshalIndent(oi, "", "	")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//fmt.Printf("%s\n", string(s))
-
-	_, err = fmt.Fprintf(f, "%s\n", s)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	fmt.Fprintf(f, "%s\n", string(jsonStr))
 }
 
-/*
-func readUnits(units interface{}, f *os.File) {
-	switch unitType := units.(type) {
-	case float64:
-		//fmt.Println("float64: ", unitType)
-		fmt.Fprintln(f, "float64: ", unitType)
-	case string:
-		fmt.Fprintln(f, "String: ", unitType)
-	case []interface{}:
-		fmt.Fprintln(f, "is an array: ", unitType)
-		for i, u := range unitType {
-			fmt.Fprint(f, i, " ")
-			readUnits(u, f)
-		}
-	case map[string]interface{}:
-		fmt.Fprintln(f, "is an object: ", unitType)
-		for k, v := range unitType {
-			fmt.Fprint(f, k, " ")
-			readUnits(v, f)
-		}
-	default:
-		fmt.Fprintln(f, "unknown type: ", unitType)
-	}
-
-}
-*/
-
-/*
-func getBodyStr(URL string) ([]byte, error) {
-	req, err := http.NewRequest("GET", URL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("User-Agent", private.USER_AGENT)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
-*/
