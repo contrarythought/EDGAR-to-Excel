ca5847bce2d271f1e4117410f6baf01fffafd987
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-17 14:45:46 -0400

    Update getInfo.go

--- EDGAR/getInfo/getInfo.go
+++ EDGAR/getInfo/getInfo.go
@@ -1,9 +1,13 @@
 package getInfo
 
 import (
+	"EDGAR/private"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -42,26 +46,30 @@ type Submissions struct {
 	StateOfIncorporationDescription   string   `json:"stateOfIncorporationDescription"`
 	Addresses                         struct {
 		Mailing struct {
-			Street1                   string `json:"street1"`
-			Street2                   string `json:"street2"`
-			City                      string `json:"city"`
-			StateOrCountry            string `json:"stateOrCountry"`
-			ZipCode                   string `json:"zipCode"`
-			StateOrCountryDescription string `json:"stateOrCountryDescription"`
+			Street1                   string      `json:"street1"`
+			Street2                   interface{} `json:"street2"`
+			City                      string      `json:"city"`
+			StateOrCountry            string      `json:"stateOrCountry"`
+			ZipCode                   string      `json:"zipCode"`
+			StateOrCountryDescription string      `json:"stateOrCountryDescription"`
 		} `json:"mailing"`
 		Business struct {
-			Street1                   string `json:"street1"`
-			Street2                   string `json:"street2"`
-			City                      string `json:"city"`
-			StateOrCountry            string `json:"stateOrCountry"`
-			ZipCode                   string `json:"zipCode"`
-			StateOrCountryDescription string `json:"stateOrCountryDescription"`
+			Street1                   string      `json:"street1"`
+			Street2                   interface{} `json:"street2"`
+			City                      string      `json:"city"`
+			StateOrCountry            string      `json:"stateOrCountry"`
+			ZipCode                   string      `json:"zipCode"`
+			StateOrCountryDescription string      `json:"stateOrCountryDescription"`
 		} `json:"business"`
 	} `json:"addresses"`
-	Phone       string   `json:"phone"`
-	Flags       string   `json:"flags,omitempty"`
-	FormerNames []string `json:"formerNames,omitifempty"`
-	Filings     struct {
+	Phone       string `json:"phone"`
+	Flags       string `json:"flags,omitempty"`
+	FormerNames []struct {
+		Name string    `json:"name"`
+		From time.Time `json:"from"`
+		To   time.Time `json:"to"`
+	}
+	Filings struct {
 		Recent struct {
 			AccessionNumber    []string    `json:"accessionNumber"`
 			FilingDate         []string    `json:"filingDate"`
@@ -86,11 +94,62 @@ type Submissions struct {
 	} `json:"filings"`
 }
 
-const URL = "https://www.sec.gov/files/company_tickers.json"
-const CIK_SIZE = 10
+const (
+	COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json"
+	CIK_SIZE            = 10
+	SUBMISSION_BASE_URL = "https://data.sec.gov/submissions/CIK"
+)
 
 func (c *CompanyCollection) FromJSON(jsonStr []byte) error {
 	return json.Unmarshal(jsonStr, &c.Collection)
+}
+
+func customGET(URL string, userAgent string) ([]byte, error) {
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", userAgent)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	ret, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Create("testGS.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(f, "%s\n", ret)
+	return ret, nil
+}
+
+/*
+	input: CIK number string that is used to set up URL for the GET request for the submissions data
+	return: pointer to Submissions struct for the inputted CIK, as well as an error
+*/
+func GetSubmissions(CIK string) (*Submissions, error) {
+	var ret = Submissions{}
+	fmt.Printf("CIK input: %s\n", CIK)
+	url_to_send := SUBMISSION_BASE_URL + CIK + ".json"
+	fmt.Printf("Sending: %s\n", url_to_send)
+	body, err := customGET(url_to_send, private.USER_AGENT)
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal into Submissions struct
+	if err = json.Unmarshal(body, &ret); err != nil {
+		return nil, err
+	}
+
+	return &ret, nil
 }
 
 func CIKToString(CIK int) string {
@@ -122,25 +181,25 @@ func initTickerToCIK() *TickerToCIK {
 	return &TickerToCIK{make(map[string]string)}
 }
 
-func DownloadTickers() (*CompanyCollection, *TickerToCIK, error) {
+func DownloadTickers() (*TickerToCIK, error) {
 	coll := &CompanyCollection{}
 
-	resp, err := http.Get(URL)
+	resp, err := http.Get(COMPANY_TICKERS_URL)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err := coll.FromJSON(body); err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	ticker_to_CIK := initTickerToCIK()
-	for k, v := range coll.Collection {
-		ticker_to_CIK.Collection[k] = CIKToString(v.CIK)
+	for _, v := range coll.Collection {
+		ticker_to_CIK.Collection[v.Ticker] = CIKToString(v.CIK)
 	}
 
-	return coll, ticker_to_CIK, nil
+	return ticker_to_CIK, nil
 }
