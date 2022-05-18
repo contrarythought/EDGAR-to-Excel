1bb516fb71564db12d07bd59d099cdcc007bd042
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-18 01:10:04 -0400

    Update getInfo.go

--- EDGAR/getInfo/getInfo.go
+++ EDGAR/getInfo/getInfo.go
@@ -3,101 +3,16 @@ package getInfo
 import (
 	"EDGAR/private"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
-	"time"
 )
 
-type CompanyTicker struct {
-	CIK    int    `json:"cik_str"`
-	Ticker string `json:"ticker"`
-	Title  string `json:"title"`
-}
-
-type CompanyCollection struct {
-	Collection map[string]CompanyTicker
-}
-
-type TickerToCIK struct {
-	Collection map[string]string
-}
-
-type Submissions struct {
-	CIK                               string   `json:"cik"`
-	EntityType                        string   `json:"entityType"`
-	SIC                               string   `json:"sic"`
-	SICDescription                    string   `json:"sicDescription"`
-	InsiderTransactionForOwnerExists  int      `json:"insiderTransactionForOwnerExists"`
-	InsiderTransactionForIssuerExists int      `json:"insiderTransactionForIssuerExists"`
-	Name                              string   `json:"name"`
-	Tickers                           []string `json:"tickers"`
-	Exchanges                         []string `json:"exchanges"`
-	EIN                               string   `json:"ein"`
-	Description                       string   `json:"description,omitempty"`
-	Website                           string   `json:"website,omitempty"`
-	InvestorWebsite                   string   `json:"investorWebsite,omitempty"`
-	Category                          string   `json:"category"`
-	FiscalYearEnd                     string   `json:"fiscalYearEnd"`
-	StateOfIncorporation              string   `json:"stateOfIncorporation"`
-	StateOfIncorporationDescription   string   `json:"stateOfIncorporationDescription"`
-	Addresses                         struct {
-		Mailing struct {
-			Street1                   string      `json:"street1"`
-			Street2                   interface{} `json:"street2"`
-			City                      string      `json:"city"`
-			StateOrCountry            string      `json:"stateOrCountry"`
-			ZipCode                   string      `json:"zipCode"`
-			StateOrCountryDescription string      `json:"stateOrCountryDescription"`
-		} `json:"mailing"`
-		Business struct {
-			Street1                   string      `json:"street1"`
-			Street2                   interface{} `json:"street2"`
-			City                      string      `json:"city"`
-			StateOrCountry            string      `json:"stateOrCountry"`
-			ZipCode                   string      `json:"zipCode"`
-			StateOrCountryDescription string      `json:"stateOrCountryDescription"`
-		} `json:"business"`
-	} `json:"addresses"`
-	Phone       string `json:"phone"`
-	Flags       string `json:"flags,omitempty"`
-	FormerNames []struct {
-		Name string    `json:"name"`
-		From time.Time `json:"from"`
-		To   time.Time `json:"to"`
-	}
-	Filings struct {
-		Recent struct {
-			AccessionNumber    []string    `json:"accessionNumber"`
-			FilingDate         []string    `json:"filingDate"`
-			ReportDate         []string    `json:"reportDate"`
-			AcceptanceDateTime []time.Time `json:"acceptanceDateTime"`
-			Act                []string    `json:"act"`
-			Form               []string    `json:"form"`
-			FileNumber         []string    `json:"fileNumber"`
-			FilmNumber         []string    `json:"filmNumber"`
-			Items              []string    `json:"items"`
-			Size               []int       `json:"size"`
-			IsXBRL             []int       `json:"isXBRL"`
-			IsInlineXBRL       []int       `json:"isInlineXBRL"`
-			PrimaryDocument    []string    `json:"primaryDocument"`
-		} `json:"recent"`
-		Files []struct {
-			Name        string `json:"name"`
-			FilingCount int    `json:"filingCount"`
-			FilingFrom  string `json:"filingFrom"`
-			FilingTo    string `json:"filingTo"`
-		} `json:"files"`
-	} `json:"filings"`
-}
-
 const (
-	COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json"
-	CIK_SIZE            = 10
-	SUBMISSION_BASE_URL = "https://data.sec.gov/submissions/CIK"
+	COMPANY_TICKERS_URL   = "https://www.sec.gov/files/company_tickers.json"
+	SUBMISSION_BASE_URL   = "https://data.sec.gov/submissions/CIK"
+	COMPANYFACTS_BASE_URL = "https://data.sec.gov/api/xbrl/companyfacts/CIK"
+	CIK_SIZE              = 10
 )
 
 func (c *CompanyCollection) FromJSON(jsonStr []byte) error {
@@ -121,13 +36,26 @@ func customGET(URL string, userAgent string) ([]byte, error) {
 		return nil, err
 	}
 
-	f, err := os.Create("testGS.txt")
+	return ret, nil
+}
+
+/*
+	input: CIK number string that is used to set up URL for the GET request for the submissions data
+	return: pointer to CompanyFacts struct for the inputted CIK, as well as an error
+*/
+func GetFacts(CIK string) (*CompanyFacts, error) {
+	var ret = CompanyFacts{}
+	url_to_send := COMPANYFACTS_BASE_URL + CIK + ".json"
+	body, err := customGET(url_to_send, private.USER_AGENT)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	fmt.Fprintf(f, "%s\n", ret)
-	return ret, nil
+	if err = json.Unmarshal(body, &ret); err != nil {
+		return nil, err
+	}
+
+	return &ret, nil
 }
 
 /*
@@ -136,9 +64,7 @@ func customGET(URL string, userAgent string) ([]byte, error) {
 */
 func GetSubmissions(CIK string) (*Submissions, error) {
 	var ret = Submissions{}
-	fmt.Printf("CIK input: %s\n", CIK)
 	url_to_send := SUBMISSION_BASE_URL + CIK + ".json"
-	fmt.Printf("Sending: %s\n", url_to_send)
 	body, err := customGET(url_to_send, private.USER_AGENT)
 	if err != nil {
 		return nil, err
