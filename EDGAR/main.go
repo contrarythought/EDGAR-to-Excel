d6ba3f669d4e7c0731545bdab4033c114dc0ad2b
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-05-19 23:34:01 -0400

    Update main.go

--- EDGAR/main.go
+++ EDGAR/main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 // example: https://data.sec.gov/api/xbrl/companyconcept/CIK##########/us-gaap/AccountsPayableCurrent.json
@@ -33,7 +35,7 @@ func main() {
 	}
 
 	tickSubmission, err := getInfo.GetFacts(tCIK.Collection[ticker])
-
+	println("Test ", tickSubmission.EntityName)
 	f, err := os.Create("res.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -45,17 +47,69 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i, s := range conceptList {
-		fmt.Println("[", i, "]", s)
-	}
+	var w sync.WaitGroup
+	var cnt int
+	for {
+		if cnt > 5 {
+			fmt.Println("Pausing for 40 seconds to ease load on server")
+			time.Sleep(40 * time.Second)
+			cnt = 0
+			fmt.Println("Back again!")
+		}
 
-	con, err := getInfo.GetConcept(tickSubmission, "Revenues")
+		for i := range conceptList {
+			fmt.Println("[", i, "] ", conceptList[i])
+		}
+
+		var c int
+		fmt.Println("Choose a concept (-1 to exit):")
+		fmt.Scanf("%d\n", &c)
+		if c == -1 {
+			break
+		}
+		fmt.Println("Building a report for: ", conceptList[c])
+
+		w.Add(1)
+		cnt++
+		fmt.Println("Reports being built: ", cnt)
+		go func(index *int) {
+			if err := BuildReport(tickSubmission, conceptList[*index]); err != nil {
+				log.Fatal(err)
+			}
+
+			w.Done()
+			fmt.Println(conceptList[*index], " report finished")
+		}(&c)
+	}
+	w.Wait()
+
+	fmt.Println("Finished!")
+}
+
+func BuildReport(facts *getInfo.CompanyFacts, concept string) error {
+	con, err := getInfo.GetConcept(facts, concept)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	for i := range con.Units.USD {
-		fmt.Fprintf(f, "Start: %s End: %s --- %d\n", con.Units.USD[i].Start, con.Units.USD[i].End, con.Units.USD[i].Val)
+	f, err := os.Create(concept + "Report.txt")
+	if err != nil {
+		return err
 	}
 
+	if len(con.Units.USD) > 0 {
+		for _, v := range con.Units.USD {
+			fmt.Fprintln(f, v.Form, " Start: ", v.Start, " End: ", v.End,
+				" ----> ", v.Val)
+		}
+	} else if len(con.Units.EUR) > 0 {
+		for _, v := range con.Units.EUR {
+			fmt.Fprintln(f, v.Form, " Start: ", v.Start, " End: ", v.End,
+				" ----> ", v.Val)
+		}
+	} else {
+		return fmt.Errorf("unsupported accounting standard")
+	}
+ 
+	return nil
 }
